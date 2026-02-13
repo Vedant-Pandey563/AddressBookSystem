ceaa1c0641b41e6e62083152860c4a6f699d85d7
Vedant Pandey <ved2003ant@gmail.com>
2026-02-13 12:26:37 +0530

    UC1 create and print contact details

---
+++ AddressBookSystem/Contact.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailId { get; set; }
+
+        
+        public Contact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNumber, string emailId)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            ZipCode = zipCode;
+            PhoneNumber = phoneNumber;
+            EmailId = emailId;
+        }
+
+        public void PrintContact()
+        {
+            Console.WriteLine("Contact Details:");
+            Console.WriteLine($"First Name  : {FirstName}");
+            Console.WriteLine($"Last Name   : {LastName}");
+            Console.WriteLine($"Address     : {Address}");
+            Console.WriteLine($"City        : {City}");
+            Console.WriteLine($"State       : {State}");
+            Console.WriteLine($"Zip Code    : {ZipCode}");
+            Console.WriteLine($"Phone Number: {PhoneNumber}");
+            Console.WriteLine($"Email Id    : {EmailId}");
+        }
+
+    }
+}
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace AddressBookSystem
+﻿
+/*
+ * Ability to create a Contacts in Address
+Book with first and last names, address,
+city, state, zip, phone number and
+email…
+ */
+
+namespace AddressBookSystem
 {
+    
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Address Book System");
+
+            
+
+            Console.WriteLine("Please enter Contact Details");
+
+            Console.WriteLine("Enter First Name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Enter Last Name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Enter Address: ");
+            string address = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Enter City Name: ");
+            string city = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Enter State Name: ");
+            string state = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Enter Zip Code: ");
+            string zipCode = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Enter Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine(); 
+
+            Console.WriteLine("Enter Email Id: ");
+            string emailId = Console.ReadLine();
+            Console.WriteLine();
+
+            Contact c = new Contact(firstName, lastName, address, city, state, zipCode, phoneNumber, emailId);
+            Console.WriteLine();
+            Console.WriteLine("Created Contact Succesfully");
+            Console.WriteLine();
+
+            c.PrintContact();
+
+
         }
     }
 }
