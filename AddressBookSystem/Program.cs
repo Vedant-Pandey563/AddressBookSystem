53f78af68a89508bc35ae589e5bd97ed698e056d
Vedant Pandey <ved2003ant@gmail.com>
2026-02-13 16:27:54 +0530

    Added UC5 -- menu driven operations , add multiple contacts-- to Main Branch

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,10 +1,6 @@
 ﻿
 /*
-Ability to add multiple
-person to Address Book
-- Use Console to add person details one at a time
-- Use Collection Class to maintain multiple
-contact persons in Address Book
+Main Branch
  */
 
 namespace AddressBookSystem
