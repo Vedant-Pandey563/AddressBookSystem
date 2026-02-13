52a0480a2a125a618d60c8601f6b35e907764a34
Vedant Pandey <ved2003ant@gmail.com>
2026-02-13 14:44:10 +0530

    Added UC3 -- find and update contact-- to UC3 branch

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -6,24 +6,46 @@ namespace AddressBookSystem
 {
     public class AddressBook
     {
-        private List<Contact> contacts;
+        private List<Contact> contacts; // creat list
 
-        public AddressBook()
+        public AddressBook() //address book constructor
         {
             contacts = new List<Contact>();
         }
 
-        public void AddContact(Contact contact)
+        public void AddContact(Contact contact) //add conctact method
         {
             contacts.Add(contact);
         }
 
-        public void PrintAddressBook()
+        public void PrintAddressBook() //print contacts from list
         {
             foreach(Contact c in contacts)
             {
                 c.PrintContact();
             }
         }
+
+        public bool EditContact(string targetName, Contact updatedContact)
+        {
+            foreach(Contact c in contacts)
+            {
+                if(c.FirstName.Equals(targetName, StringComparison.OrdinalIgnoreCase))
+                {
+                    c.LastName = updatedContact.LastName;
+                    c.Address = updatedContact.Address;
+                    c.City = updatedContact.City;
+                    c.State = updatedContact.State;
+                    c.ZipCode = updatedContact.ZipCode;
+                    c.PhoneNumber = updatedContact.PhoneNumber;
+                    c.EmailId = updatedContact.EmailId;
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
     }
 }
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,6 +1,9 @@
 ﻿
 /*
- * Main branch
+ * Ability to edit
+existing contact
+person using their
+name - Use Console to edit person details
  */
 
 namespace AddressBookSystem
@@ -8,52 +11,79 @@ namespace AddressBookSystem
     
     internal class Program
     {
+        static void GetDetails(Contact c) // method to get contact details
+        {
+            Console.WriteLine("Please enter Contact Details");
+
+           Console.Write("Enter First Name: ");
+            c.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            c.LastName = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            c.Address = Console.ReadLine();
+
+            Console.Write("Enter City Name: ");
+            c.City = Console.ReadLine();
+
+            Console.Write("Enter State Name: ");
+            c.State = Console.ReadLine();
+
+            Console.Write("Enter Zip Code: ");
+            c.ZipCode = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            c.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email Id: ");
+            c.EmailId = Console.ReadLine();
+
+        }
+
+        // method to update target contact details
+        static void UpdateContact(AddressBook addressBook) // passing addressbook obj to modfify contact in it
+        {
+            Console.WriteLine("Enter name of target contact : ");
+            string target = Console.ReadLine();
+
+            Contact tempContact = new Contact(target, "", "", "", "", "", "", "");
+            GetDetails(tempContact);
+
+            bool isUpdated = addressBook.EditContact(target, tempContact);
+
+            if (isUpdated)
+            {
+                Console.WriteLine("\nContact updated successfully!");
+            }
+            else
+            {
+                Console.WriteLine("\nContact not found.");
+            }
+
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Address Book System");
 
-            Console.WriteLine("Please enter Contact Details");
-
-            Console.WriteLine("Enter First Name: ");
-            string firstName = Console.ReadLine();
-            Console.WriteLine();
-
-            Console.WriteLine("Enter Last Name: ");
-            string lastName = Console.ReadLine();
-            Console.WriteLine();
-
-            Console.WriteLine("Enter Address: ");
-            string address = Console.ReadLine();
-            Console.WriteLine();
-
-            Console.WriteLine("Enter City Name: ");
-            string city = Console.ReadLine();
-            Console.WriteLine();
-
-            Console.WriteLine("Enter State Name: ");
-            string state = Console.ReadLine();
-            Console.WriteLine();
-
-            Console.WriteLine("Enter Zip Code: ");
-            string zipCode = Console.ReadLine();
-            Console.WriteLine();
-
-            Console.WriteLine("Enter Phone Number: ");
-            string phoneNumber = Console.ReadLine();
-            Console.WriteLine(); 
-
-            Console.WriteLine("Enter Email Id: ");
-            string emailId = Console.ReadLine();
-            Console.WriteLine();
-
+            
             AddressBook addressBook = new AddressBook();
-            Contact c = new Contact(firstName, lastName, address, city, state, zipCode, phoneNumber, emailId);
+            Contact c = new Contact("", "", "", "", "", "", "", "");
+
+            GetDetails(c);
+
             Console.WriteLine();
             Console.WriteLine("Created Contact Succesfully");
             Console.WriteLine();
 
             addressBook.AddContact(c);
+
             Console.WriteLine("Contact added successfully to Address Book.");
+            addressBook.PrintAddressBook();
+
+            UpdateContact(addressBook);
+
             addressBook.PrintAddressBook();
 
 
