20310c521842c009eb5459c0a90f82f01f5bdc98
Vedant Pandey <ved2003ant@gmail.com>
2026-03-19 20:55:37 +0530

    Added Modular based changes to code

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -1,256 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AddressBookSystem
+﻿namespace AddressBookSystem
 {
     public class AddressBook
     {
-        private List<Contact> contacts; // creat list
+        private List<Contact> contacts = new();
+        private Dictionary<string, List<Contact>> cityDict = new();
+        private Dictionary<string, List<Contact>> stateDict = new();
 
-        //dicts of city and state
-        private Dictionary<string, List<Contact>> cityDictionary;
-        private Dictionary<string, List<Contact>> stateDictionary;
-
-        public AddressBook() //address book constructor
+        public bool AddContact(Contact c)
         {
-            contacts = new List<Contact>();
-            cityDictionary = new Dictionary<string, List<Contact>>();
-            stateDictionary = new Dictionary<string, List<Contact>>();
-        }
+            if (contacts.Contains(c)) return false;
 
-        public bool AddContact(Contact contact) //add conctact method
-        {
-            if (contacts.Contains(contact)) // conatains uses equals which is overriden
-            {
-                return false;
-            }
+            contacts.Add(c);
 
-            contacts.Add(contact);
-            //update city dict
-            if (!cityDictionary.ContainsKey(contact.City))
-            {
-                cityDictionary[contact.City] = new List<Contact>();
-            }
-            cityDictionary[contact.City].Add(contact);
-            //update state dict 
-            if (!stateDictionary.ContainsKey(contact.State))
-            {
-                stateDictionary[contact.State] = new List<Contact>();
-            }
-            stateDictionary[contact.State].Add(contact);
+            if (!cityDict.ContainsKey(c.City))
+                cityDict[c.City] = new List<Contact>();
+            cityDict[c.City].Add(c);
+
+            if (!stateDict.ContainsKey(c.State))
+                stateDict[c.State] = new List<Contact>();
+            stateDict[c.State].Add(c);
+
             return true;
-
         }
 
-        public void PrintAddressBook() //print contacts from list
+        public bool EditContact(string name, Contact updated)
         {
-            if (contacts.Count == 0)
-            {
-                Console.WriteLine("Address Book is Empty");
-                return;
-            }
-            int i = 1;
-            foreach (Contact c in contacts)
-            {
-                Console.Write("Contact No. " + i);
-                c.PrintContact();
-                i++;
-            }
+            var c = contacts.FirstOrDefault(x =>
+                x.FirstName.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+            if (c == null) return false;
+
+            cityDict[c.City].Remove(c);
+            stateDict[c.State].Remove(c);
+
+            c.LastName = updated.LastName;
+            c.Address = updated.Address;
+            c.City = updated.City;
+            c.State = updated.State;
+            c.ZipCode = updated.ZipCode;
+            c.PhoneNumber = updated.PhoneNumber;
+            c.EmailId = updated.EmailId;
+
+            AddContact(c);
+            return true;
         }
 
-        //method to print contacts by cities
-        public void ViewByCity(string city)
+        public bool DeleteContact(string name)
         {
-            if (cityDictionary.ContainsKey(city))
-            {
-                foreach (Contact c in cityDictionary[city])
-                {
-                    c.PrintContact();
-                }
-            }
-            else
-            {
-                Console.WriteLine("No contacts found in this city.");
-            }
+            var c = contacts.FirstOrDefault(x =>
+                x.FirstName.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+            if (c == null) return false;
+
+            contacts.Remove(c);
+            cityDict[c.City].Remove(c);
+            stateDict[c.State].Remove(c);
+
+            return true;
         }
 
-        //same for states 
-        public void ViewByState(string state)
-        {
-            if (stateDictionary.ContainsKey(state))
-            {
-                foreach (Contact c in stateDictionary[state])
-                {
-                    c.PrintContact();
-                }
-            }
-            else
-            {
-                Console.WriteLine("No contacts found in this state.");
-            }
-        }
+        public List<Contact> GetAllContacts() => contacts;
 
+        public List<Contact> GetByCity(string city) =>
+            cityDict.ContainsKey(city) ? cityDict[city] : new();
 
-        // inlist method removed , but how it is there in uc8,9 but not in uc7 
-        //should recheck
-        public bool EditContact(string targetName, Contact updatedContact)
-        {
-            foreach (Contact c in contacts)
-            {
-                if (c.FirstName.Equals(targetName, StringComparison.OrdinalIgnoreCase))
-                {
+        public List<Contact> GetByState(string state) =>
+            stateDict.ContainsKey(state) ? stateDict[state] : new();
 
+        public int GetCountByCity(string city) =>
+            cityDict.ContainsKey(city) ? cityDict[city].Count : 0;
 
-                    //remove old dict entries for city,state 
-                    cityDictionary[c.City].Remove(c);
-                    stateDictionary[c.State].Remove(c);
+        public int GetCountByState(string state) =>
+            stateDict.ContainsKey(state) ? stateDict[state].Count : 0;
 
-                    //now update
-                    c.LastName = updatedContact.LastName;
-                    c.Address = updatedContact.Address;
-                    c.City = updatedContact.City;
-                    c.State = updatedContact.State;
-                    c.ZipCode = updatedContact.ZipCode;
-                    c.PhoneNumber = updatedContact.PhoneNumber;
-                    c.EmailId = updatedContact.EmailId;
+        public List<Contact> GetSortedByName() =>
+            contacts.OrderBy(c => c.FirstName).ToList();
 
-                    //now add updated back to dicts 
-                    if (!cityDictionary.ContainsKey(c.City))
-                        cityDictionary[c.City] = new List<Contact>();
-                    cityDictionary[c.City].Add(c);
+        public List<Contact> GetSortedByCity() =>
+            contacts.OrderBy(c => c.City).ToList();
 
-                    if (!stateDictionary.ContainsKey(c.State))
-                        stateDictionary[c.State] = new List<Contact>();
-                    stateDictionary[c.State].Add(c);
+        public List<Contact> GetSortedByState() =>
+            contacts.OrderBy(c => c.State).ToList();
 
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        public bool DeleteContact(string targetName)
-        {
-            for (int i = 0; i < contacts.Count; i++)//foreach loop gives synatx error
-            {
-                if (contacts[i].FirstName.Equals(targetName, StringComparison.OrdinalIgnoreCase))
-                {
-                    Contact c = contacts[i];
-                    cityDictionary[c.City].Remove(c);
-                    stateDictionary[c.State].Remove(c);
-                    contacts.RemoveAt(i);
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        // city searcher method
-        public List<Contact> SearchByCity(string city)
-        {
-            List<Contact> result = new List<Contact>();
-
-            foreach (Contact c in contacts)
-            {
-                if (c.City.Equals(city, StringComparison.OrdinalIgnoreCase))
-                {
-                    result.Add(c);
-                }
-            }
-
-            return result;
-        }
-
-        //state searcher method 
-        public List<Contact> SearchByState(string state)
-        {
-            List<Contact> result = new List<Contact>();
-
-            foreach (Contact c in contacts)
-            {
-                if (c.State.Equals(state, StringComparison.OrdinalIgnoreCase))
-                {
-                    result.Add(c);
-                }
-            }
-
-            return result;
-        }
-
-        //count of city 
-        public int GetCountByCity(string city)
-        {
-            if (cityDictionary.ContainsKey(city))
-                return cityDictionary[city].Count;
-
-            return 0;
-        }
-
-        //count of state 
-        public int GetCountByState(string state)
-        {
-            if (stateDictionary.ContainsKey(state))
-                return stateDictionary[state].Count;
-
-            return 0;
-        }
-
-        //sort by name 
-        public void SortByName()
-        {
-            contacts.Sort((c1, c2) =>
-                string.Compare(c1.FirstName, c2.FirstName, StringComparison.OrdinalIgnoreCase));
-
-            Console.WriteLine("Contacts sorted alphabetically by First Name:\n");
-
-            foreach (Contact c in contacts)
-            {
-                Console.WriteLine(c); // Uses ToString()
-            }
-        }
-
-        public void SortByCity()
-        {
-            contacts.Sort((c1, c2) =>
-                string.Compare(c1.City, c2.City, StringComparison.OrdinalIgnoreCase));
-
-            Console.WriteLine("\nContacts sorted by City:\n");
-
-            foreach (Contact c in contacts)
-            {
-                Console.WriteLine(c); // uses ToString()
-            }
-        }
-
-        public void SortByState()
-        {
-            contacts.Sort((c1, c2) =>
-                string.Compare(c1.State, c2.State, StringComparison.OrdinalIgnoreCase));
-
-            Console.WriteLine("\nContacts sorted by State:\n");
-
-            foreach (Contact c in contacts)
-            {
-                Console.WriteLine(c);
-            }
-        }
-
-        public void SortByZip()
-        {
-            contacts.Sort((c1, c2) =>
-                string.Compare(c1.ZipCode, c2.ZipCode, StringComparison.OrdinalIgnoreCase));
-
-            Console.WriteLine("\nContacts sorted by Zip Code:\n");
-
-            foreach (Contact c in contacts)
-            {
-                Console.WriteLine(c);
-            }
-        }
-
-
+        public List<Contact> GetSortedByZip() =>
+            contacts.OrderBy(c => c.ZipCode).ToList();
     }
 }
